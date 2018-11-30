95458781eeb6f8bca62fae2cafdebc945fd0c3d1
jorcuad <jorgecuadradosaez@gmail.com>
2018-11-30 16:18:06 +0100

    nav menu, icons style update

---
+++ src/components/SettingNav.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .setting-nav
+    span
+      | Settings
+</template>
+
+<script>
+
+export default {
+  name: 'SettingNav',
+}
+</script>
+
+<style lang="sass">
+  .setting-nav
+    background-color: rgba(142,142,147,0.9)
+    width: 100%
+    height: 100%
+    padding: 10px
+    border-left: 2px solid rgba(105,105,110,0.9)
+
+</style>
