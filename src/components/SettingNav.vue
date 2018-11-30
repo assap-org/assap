c027afa60273c0b2d41871420b2224e8a68aac72
jorcuad <jorgecuadradosaez@gmail.com>
2018-11-30 16:57:53 +0100

    setting nav styles

--- src/components/SettingNav.vue
+++ src/components/SettingNav.vue
@@ -1,7 +1,18 @@
 <template lang="pug">
   .setting-nav
-    span
-      | Settings
+    .settings-header
+      h1
+        | Settings
+    .options
+      .opt-section
+        span
+          | Train Model
+      .opt-section
+        span
+          | Load loadURL
+      .opt-section
+        span
+          | Actions
 </template>
 
 <script>
@@ -13,10 +24,31 @@ export default {
 
 <style lang="sass">
   .setting-nav
-    background-color: rgba(142,142,147,0.9)
+    background-color: rgba(142,142,147,1)
     width: 100%
-    height: 100%
-    padding: 10px
+    height: 150px
+    overflow-y: scroll
     border-left: 2px solid rgba(105,105,110,0.9)
+
+    .settings-header
+      position: fixed
+      top: 0
+      padding: 10px
+      width: 100%
+      height: 45px
+      background-color: rgb(105,105,110)
+      h1
+        font-weight: bold
+        color: white
+  .options
+    width: 100%
+    margin: 0
+    margin-top: 30px
+    padding: 10px
+  .opt-section
+    margin-top: 15px
+    span
+      font-weight: bold
+      color: white
 
 </style>
