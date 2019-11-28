6afeb2af2ae24d63ad890d39f281223b75c51516
RuthGnz <ruthgonznov@gmail.com>
2019-11-28 12:47:18 +0100

    random salt and function doc

--- src/utils/cipher.js
+++ src/utils/cipher.js
@@ -1,13 +1,34 @@
-// This file contains all code necessary to cipher all the configuration in the ASSAP tool.
-// Having all the configuration encrypted is a security requirement. It prevents an attacker from modifying the identification
-// configuration of the ASSAP tool. For this purpose we use AES and GCM.
+/**
+  * This file contains all code necessary to cipher all the configuration in the ASSAP tool.
+  * Having all the configuration encrypted is a security requirement. It prevents an attacker from modifying the identification
+  * configuration of the ASSAP tool. For this purpose we use AES and GCM.
+  */
+
 
 const crypto = require('crypto');
 const algorithm = 'aes-256-gcm';
 
+/**
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
 
+
+/**
+ * Encrypts data using aes-256-gcm, a random salt and a random iv.
+ * @function
+ * @param {String} text - Text to encrypt
+ * @param {String} password - User password
+ */
 function encrypt(text,password) {
-  const key = crypto.scryptSync(password, 'salt', 32);
+  const salt = genRandomString(16) // It is recommended that a salt is random and at least 16 bytes long.
+  const key = crypto.scryptSync(password, salt, 32)
   const iv = crypto.randomBytes(16); // Initialization vector.
   const cipher = crypto.createCipheriv(algorithm, key, iv);
   var encrypted = cipher.update(text, 'utf8', 'hex')
@@ -16,12 +37,20 @@ function encrypt(text,password) {
   return {
     content: encrypted,
     tag: tag,
-    iv: iv
+    iv: iv,
+    salt: salt
   };
 }
 
+
+/**
+ * Decrypts data using aes-256-gcm, a random salt and a random iv.
+ * @function
+ * @param {String} encrypted - Text to dencrypt
+ * @param {String} password - User password
+ */
 function decrypt(encrypted,password) {
-  const key = crypto.scryptSync(password, 'salt', 32);
+  const key = crypto.scryptSync(password, encrypted.salt, 32);
   const decipher = crypto.createDecipheriv(algorithm, key, encrypted.iv);
   decipher.setAuthTag(encrypted.tag);
   let decrypted = decipher.update(encrypted.content, 'hex', 'utf8');
